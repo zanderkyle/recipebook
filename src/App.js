5627a2ebc2438d1a1a4c7a0777a33c2057948967
Kyle Gonyea <kgonyea@evolutionnutrition.com>
2023-10-22 11:22:36 -0700

    add basic router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Dashboard from './components/Dashboard/Dashboard';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Router>
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+        </Router>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
