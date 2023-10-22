2f886cf904f2fb5ef40bd024fc1d9bc51c31486c
Kyle Gonyea <kgonyea@evolutionnutrition.com>
2023-10-22 09:40:53 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
+    
     <div className="App">
+    <Router>
+    <Routes>
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+    </Router>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -15,7 +23,28 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React1
+        </a>
+      </header>
+    </div>
+  );
+}
+
+function Dashboard() {
+  return (
+    <div className="Dashboard">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit2 <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React2
         </a>
       </header>
     </div>
